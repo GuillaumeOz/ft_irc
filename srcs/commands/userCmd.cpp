f19856657200d6f2e78bb36919ba1636728d23d8
nabitbol <abitbol.noam@protonmail.com>
2022-03-03 16:48:26 +0100

    ✨(user_command): add user command

---
+++ srcs/commands/userCmd.cpp
@@ -0,0 +1,39 @@
+#include "ft_irc.hpp"
+
+std::string		goToUserCmd(std::string &command) {
+	size_t i = command.find("USER");
+
+	if (i != std::string::npos) {
+		std::string newString;
+
+		newString += command.c_str() + i;
+		return (newString);
+	}
+	return (command);
+}
+
+void	userCmd(Server &server, int index, std::string &command) {
+	std::string cmd;
+	std::string tmp;
+	char *ptr;
+	int i = {0};
+
+	cmd = goToUserCmd(command);
+	if (strncmp(cmd.c_str(), "USER", 4 ) == 0) {
+		ptr = strtok(const_cast<char *>(cmd.c_str()), " ");
+		while (ptr != NULL && i < 4) {
+			ptr = strtok(NULL, " :\r");
+			tmp = ptr;
+			POUT(tmp)
+			if (i == 0)
+				server.setUsername(index, tmp);
+			if (i == 2)
+				server.setHost(index, tmp);
+			if (i == 3)
+				server.setRealname(index, tmp);
+			i++;
+		}
+		if (i < 3)
+			server.sendError(command.c_str(), NULL, NULL, ERR_NEEDMOREPARAMS, index);
+	}
+}
