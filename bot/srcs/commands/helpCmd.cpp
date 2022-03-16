9882190d58353fc52400dbfd025f511f08b9adb9
Thibaut Gontier <thibautgontier@Thibauts-MacBook-Pro.local>
2022-03-16 10:59:19 +0100

    ✨added commands to bot

--- bot/srcs/commands/helpCmd.cpp
+++ bot/srcs/commands/helpCmd.cpp
@@ -2,6 +2,6 @@
 
 void    helpCmd(Client &client, std::string &string) {
     (void)string;
-    std::string help("PRIVMSG #bot :The avaliable commands are:\n- !creators -> get beautiful ascii art of the creators of this project.\n- !ascii -> get beautiful ascii art of the 42 logo.\n");
+    std::string help("PRIVMSG #bot :The avaliable commands are:\nPRIVMSG #bot :- !creators -> get beautiful ascii art of the creators of this project.\nPRIVMSG #bot :- !ascii -> get beautiful ascii art of the 42 logo.\n");
     client.bsend(help);
 }
